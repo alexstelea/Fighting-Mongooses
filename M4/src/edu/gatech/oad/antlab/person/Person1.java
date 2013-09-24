d909e45b1f1edb54569e9de3c879d8d43980ba06
Orion - Kennon Bittick <kennonlbc@gmail.com>
2013-09-24 16:48:40 -0400

    Did Person1 file

--- M4/src/edu/gatech/oad/antlab/person/Person1.java
+++ M4/src/edu/gatech/oad/antlab/person/Person1.java
@@ -31,6 +31,7 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
+      String toReturn = input.substring(2, input.length()) + input.substring(0, 2);
 	  return null;
 	}
 	
