fb8d6d8cd70d755bc3a3f0c080886df13a2bb511
Orion - Kennon Bittick <kennonlbc@gmail.com>
2013-09-26 12:15:54 -0400

    Fixed typo in AntLabMain.java

--- M4/src/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M4/src/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -59,7 +59,7 @@ import edu.gatech.oad.antlab.pkg2.*;
           + ant22.getMessage() + ant31.getMessage();
 		  //Person1 replace P1 with your name
 		  //and ggg000x with your gt num
-		  Person1 = new Person1("Kennon Bittick");
+		  Person1 p1 = new Person1("Kennon Bittick");
 		  toPrint += p1.toString("902685827");
 		  //Person2 replace P2 with your name
 		  //and ggg000x with your gt num
