328d4f325490067fff31e94b10946af96083b7af
Tyler <tmacgrogan3@gatech.edu>
2013-10-01 17:46:37 -0400

    Added startup screen model

---
+++ MULE/src/startScreenModel.java
@@ -0,0 +1,13 @@
+public startScreenModel extends Model {
+
+	private Boolean isSavedGameAvailable = False; 
+	
+	public getIsSavedGameAvailable() {
+		return isSavedGameAvailable;
+	}
+	
+	public setIsSavedGameAvailable(Boolean isSavedGameAvailable) {
+		this.isSavedGameAvailable = isSavedGameAvailable;
+	}
+	
+}
