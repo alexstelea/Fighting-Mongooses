40c3dcb6d4f26c389801712aef0e371cce6a3251
Alex Stelea <alex.stelea@gmail.com>
2013-10-01 17:49:05 -0400

    added gamecontroller

--- MULE/src/GameController.java
+++ MULE/src/GameController.java
@@ -1,5 +1,6 @@
 public class GameController {
 
+<<<<<<< Updated upstream
     /**
      * Main method of the MULE game
      * Initializes the environment and starts the game
@@ -13,3 +14,36 @@ public class GameController {
     }
 }
 
+=======
+	public static final int EASY_DIFFICULTY = 1;
+	public static final int MEDIUM_DIFFICULTY = 2;
+	public static final int HARD_DIFFICULTY = 3;
+	public static final int DIFFICULTY_NOT_SET = 0;
+
+	private int difficulty;
+	private Renderer renderer;
+
+
+	public GameController() {
+		renderer = new Renderer();
+		SaveGame[] savedGames = getSavedGames();
+		Model startScreenModel = new StartScreenModel();
+		if(savedGames == null) {
+			startScreenModel.setIsSavedGameAvailible(true);
+		}
+		showStartScreen(startScreenModel);
+	}
+
+	private void showStartScreen(Model model) {
+		renderer.drawStartScreen(model)
+	}
+
+	private SaveGame[] getSavedGames() {
+		//Query database for saved games
+	}
+
+	public int getDifficulty() {
+		return difficulty;
+	}
+}
+>>>>>>> Stashed changes
