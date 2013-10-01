af99927e18ab716d5d77b6fb52ce3d37f1fa1bbd
Tyler <tmacgrogan3@gatech.edu>
2013-10-01 17:33:33 -0400

    Added models

---
+++ MULE/src/Renderer.java
@@ -0,0 +1,29 @@
+import models.*;
+
+public class Renderer {
+
+	//instance variables
+	public Renderer() {
+	//
+	}
+	
+	public void drawStartScreen(startScreenModel model){
+		
+	}
+	
+	public void difficultyScreen(Model model) {
+	
+	}
+	
+	public void playerScreen(Model model) {
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
