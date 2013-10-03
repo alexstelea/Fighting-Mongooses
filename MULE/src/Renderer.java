d61371577b64787517d6345762b574e70785430e
Geo <geoving.gerard@gmail.com>
2013-10-03 18:42:06 -0400

    Additional GUI changes
    -Transparent buttons
    -Small tweaks/fixes
    -No errors

--- MULE/src/Renderer.java
+++ MULE/src/Renderer.java
@@ -14,15 +14,15 @@ import javax.swing.JPanel;
 public class Renderer implements MouseListener {
 
 	//instance variables
-	public static  final String start = "Start";
-	public static final String difficulty = "Difficulty";
-	public static final String map = "Map";
-	public static final String player = "Player";
+	static final String start = "Start";
+	static final String setup = "Setup";
+	static final String map = "Map";
+	static final String player = "Player";
+	static final String mainScreen = "mainScreen";
 
 	private JPanel panel;
 
 	public Renderer() {
-
 
 		JFrame frame = new JFrame("M.U.L.E.");
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -41,97 +41,159 @@ public class Renderer implements MouseListener {
 		Container pane = new Container();
 
 		// JPanel CardStart = new ImagePanel();
-	 //    JPanel CardDiff = new DifficultyImagePanel();
-	 //    JPanel CardMap = new MapImagePanel();
-	 //    JPanel CardPlayer = new PlayerImagePanel();
+	    // JPanel CardSetup = new SetupImagePanel();
+	    // JPanel CardMap = new MapImagePanel();
+	    // JPanel CardPlayer = new PlayerImagePanel();
+	    // JPanel CardMainScreen = ;
 
 	
 	    panel = new JPanel(new CardLayout());
 	    // panel.add(CardStart, start);
-	    // panel.add(CardDiff, difficulty);
+	    // panel.add(CardSetup, setup);
 	    // panel.add(CardMap, map);
 	    // panel.add(CardPlayer, player);
-
+	    // panel.add(CardMainScreen, mainScreen);
 	
 	    Pane.add(panel, BorderLayout.CENTER);
 	
-	
-	    //CardStart
+	    // CardStart
 	    // CardStart.setLayout(null);
-	    // CardDiff.setLayout(null);
+	    // CardSetup.setLayout(null);
 	    // CardMap.setLayout(null);
 	    // CardPlayer.setLayout(null);
-	    
-	
+	    // CardMainScreen.setLayout(null);
+	   
 	    //Quit
-	    JButton quit = new JButton("Quit");
+	    JButton quit = new JButton("");
 	    quit.addMouseListener(new MouseAdapter() {
 	        public void mouseClicked(MouseEvent e) {
 	        	System.exit(0);
 	        }
 	    });
+	    quit.setOpaque(false);
+	    quit.setContentAreaFilled(false);
+	    quit.setBorderPainted(false);
 	    quit.setBounds(11, 536, 171, 40);
 	    // CardStart.add(quit);
 	    
 	    //Load
-	    JButton load = new JButton("Load");
+	    JButton load = new JButton("");
 	    load.addMouseListener(new MouseAdapter() {
 	        public void mouseClicked(MouseEvent e) {
 	            System.out.println("Load");
 	            /*
 	            CardLayout cl = (CardLayout) (panel.getLayout());
-	            cl.show(panel, Difficulty);
+	            cl.show(panel, Setup);
 	            */
 	        }
 	    });
+	    load.setOpaque(false);
+	    load.setContentAreaFilled(false);
+	    load.setBorderPainted(false);
 	    load.setBounds(590, 536, 170, 40);
 	    // CardStart.add(load);
 	    
 	    //NewGame
-	    JButton newGame = new JButton("New Game");
+	    JButton newGame = new JButton("");
 	    newGame.addMouseListener(new MouseAdapter() {
 	        public void mouseClicked(MouseEvent e) {
-	            System.out.println("New Game");
 	            CardLayout cl = (CardLayout) (panel.getLayout());
-	            cl.show(panel, difficulty);
+	            cl.show(panel, setup);
 	        }
 	    });
+	    newGame.setOpaque(false);
+	    newGame.setContentAreaFilled(false);
+	    newGame.setBorderPainted(false);
 	    newGame.setBounds(771, 536, 170, 40);
 	    // CardStart.add(newGame);
 	    
-	    //Go back!
-	    JButton goBack = new JButton("Go Back!");
+	    //Go back! (Setup Page)
+	    JButton goBack = new JButton("");
 	    goBack.addMouseListener(new MouseAdapter() {
 	        public void mouseClicked(MouseEvent e) {
 	            CardLayout cl = (CardLayout) (panel.getLayout());
 	            cl.show(panel, start);
 	        }
 	    });
+	    goBack.setOpaque(false);
+	    goBack.setContentAreaFilled(false);
+	    goBack.setBorderPainted(false);
 	    goBack.setBounds(11, 536, 170, 40);
-	    // CardDiff.add(goBack);
+	    // CardSetup.add(goBack);
 	    
-	    //Okay!
-	    JButton okay = new JButton("Okay!");
+	    //Okay! (Setup Page)
+	    JButton okay = new JButton("");
 	    okay.addMouseListener(new MouseAdapter() {
 	        public void mouseClicked(MouseEvent e) {
-	            System.out.println("Okay!");
 	            CardLayout cl = (CardLayout) (panel.getLayout());
 	            cl.show(panel, map);
 	        }
 	    });
+	    okay.setOpaque(false);
+	    okay.setContentAreaFilled(false);
+	    okay.setBorderPainted(false);
 	    okay.setBounds(771, 536, 170, 40);
-	    // CardDiff.add(okay);
+	    // CardSetup.add(okay);
 	    
-	    //Go back! (Map)
-	    JButton goBackMap = new JButton("Go Back!");
+	    //Go back! (Map Page)
+	    JButton goBackMap = new JButton("");
 	    goBackMap.addMouseListener(new MouseAdapter() {
 	        public void mouseClicked(MouseEvent e) {
 	            CardLayout cl = (CardLayout) (panel.getLayout());
-	            cl.show(panel, difficulty);
+	            cl.show(panel, setup);
 	        }
 	    });
+	    goBackMap.setOpaque(false);
+	    goBackMap.setContentAreaFilled(false);
+	    goBackMap.setBorderPainted(false);
 	    goBackMap.setBounds(11, 536, 170, 40);
 	    // CardMap.add(goBackMap);
+
+	   	//Okay! (Map Page)
+	    JButton okayMap = new JButton("");
+	    okayMap.addMouseListener(new MouseAdapter() {
+	        public void mouseClicked(MouseEvent e) {
+	            CardLayout cl = (CardLayout) (panel.getLayout());
+	            cl.show(panel, player);
+	        }
+	    });
+	    okayMap.setOpaque(false);
+	    okayMap.setContentAreaFilled(false);
+	    okayMap.setBorderPainted(false);
+	    okayMap.setBounds(771, 536, 170, 40);
+	    //CardMap.add(okayMap);
+
+	    //Go back! (Player Page)
+	    JButton goBackPlayer = new JButton("");
+	    goBackPlayer.addMouseListener(new MouseAdapter() {
+	        public void mouseClicked(MouseEvent e) {
+	            CardLayout cl = (CardLayout) (panel.getLayout());
+	            cl.show(panel, map);
+	        }
+	    });
+	    goBackPlayer.setOpaque(false);
+	    goBackPlayer.setContentAreaFilled(false);
+	    goBackPlayer.setBorderPainted(false);
+	    goBackPlayer.setBounds(11, 536, 170, 40);
+	    //CardPlayer.add(goBackPlayer);
+
+	    //Okay! (Player Page)
+	    JButton okayPlayer = new JButton("");
+	    okayPlayer .addMouseListener(new MouseAdapter() {
+	        public void mouseClicked(MouseEvent e) {
+	        	/*
+	            System.out.println("Okay!");
+	            CardLayout cl = (CardLayout) (panel.getLayout());
+	            cl.show(panel, mainScreen);
+	        	 */
+	    	System.out.println("Okay!");
+	        }
+	    });
+	    okayPlayer.setOpaque(false);
+	    okayPlayer.setContentAreaFilled(false);
+	    okayPlayer.setBorderPainted(false);
+	    okayPlayer.setBounds(771, 536, 170, 40);
+	    //CardPlayer.add(okayPlayer);
 	}
 	
 	public void drawStartScreen(StartScreenModel model){
@@ -155,7 +217,7 @@ public class Renderer implements MouseListener {
 		//call action
 	}
 	
-	public void difficultyScreen(Model model) {
+	public void setupScreen(Model model) {
 	
 	}
 	
