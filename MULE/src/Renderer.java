303bd5c47a26151594186e5869d49c3e6ba8fb0a
Geo <geoving.gerard@gmail.com>
2013-11-26 14:50:59 -0500

    ...

--- MULE/src/Renderer.java
+++ MULE/src/Renderer.java
@@ -12,9 +12,6 @@ import java.awt.*;
 import java.awt.font.*;
 import java.awt.image.BufferedImage;
 
-import java.io.File;
-import java.io.IOException;
-
 import javax.imageio.ImageIO;
 
 import javax.swing.*;
